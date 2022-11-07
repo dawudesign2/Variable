dbf71137694e94169d4a9668446f0a453cf3f5a2
Dawudesign <contact@dawudesign.fr>
2022-11-07 14:35:06 +0100

    change2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String movieTitle = "Indiana Jones and the Last Crusade";
-        Boolean youSawIt = true;
-        Integer movieYear = 1989;
-        Double movieRating = 8.2;
+        boolean youSawIt = true;
+        int movieYear = 1989;
+        double movieRating = 8.2;
 
         System.out.println(
                 "the title of the movie : " + movieTitle +
