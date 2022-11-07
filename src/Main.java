9b7d5c24f949c4fff0318c2e542c07bb55023a22
Dawudesign <contact@dawudesign.fr>
2022-11-07 14:34:08 +0100

    change

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
         Integer movieYear = 1989;
         Double movieRating = 8.2;
 
-        System.out.println(movieTitle );
-        System.out.println(youSawIt);
-        System.out.println(movieYear);
-        System.out.println(movieRating);
+        System.out.println(
+                "the title of the movie : " + movieTitle +
+                        "You saw it ? : " + youSawIt +
+                        " the year of the movie :" + movieYear +
+                        " His rating" + movieRating + "/10");
     }
 }
