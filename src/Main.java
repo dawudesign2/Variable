cb5b7119deb0b4cc868ea9ea7ca50aaef9a55035
Dawudesign <contact@dawudesign.fr>
2022-11-07 14:24:50 +0100

    start

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String movieTitle = "Indiana Jones and the Last Crusade";
+        Boolean youSawIt = true;
+        Integer movieYear = 1989;
+        Double movieRating = 8.2;
+
+        System.out.println(movieTitle );
+        System.out.println(youSawIt);
+        System.out.println(movieYear);
+        System.out.println(movieRating);
+    }
+}
